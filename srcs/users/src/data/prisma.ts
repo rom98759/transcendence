3def7d0bbe6d1c771cfaf9623f19a52244a9af18
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-12-20 16:31:47 +0100

    fix: yet another import

--- srcs/users/src/data/prisma.ts
+++ srcs/users/src/data/prisma.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import { appenv } from '../config/env.js'
-import { PrismaClient } from './generated/prisma/client.js'
+import { PrismaClient } from '@prisma/client'
 
 const connectionString = appenv.UM_DB_URL
 
