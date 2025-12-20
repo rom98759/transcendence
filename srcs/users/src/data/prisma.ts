31ef11f146f796de4d8a325583217b0b44f851cc
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-12-20 15:12:57 +0100

    fix: include data folder from service

---
+++ srcs/users/src/data/prisma.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { appenv } from '../config/env.js'
+import { PrismaClient } from './generated/prisma/client.js'
+
+const connectionString = appenv.UM_DB_URL
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
---
+++ srcs/users/src/data/um.data.ts
@@ -0,0 +1,25 @@
+import { UserProfile } from './generated/prisma/client.js'
+import { prisma } from './prisma.js'
+
+export async function findProfileByUsername(
+  username: string
+): Promise<UserProfile | null> {
+  return await prisma.userProfile.findUnique({
+    where: { username },
+  })
+}
+
+export async function createProfile(data: {
+  authId: number
+  email: string
+  username: string
+}): Promise<UserProfile> {
+  return await prisma.userProfile.create({
+    data: {
+      authId: data.authId,
+      email: data.email,
+      username: data.username,
+      avatarUrl: "assets/avatars/default.png"
+    },
+  })
+}
